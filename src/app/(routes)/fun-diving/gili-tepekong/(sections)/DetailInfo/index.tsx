b37a5263dcb49d61db3a0a27e95a09352138d05c
dhantosy <dhantosy@gmail.com>
2023-10-31 21:08:36 +0800

    feat: added gili tepekong page

---
+++ src/app/(routes)/fun-diving/gili-tepekong/(sections)/DetailInfo/index.tsx
@@ -0,0 +1,167 @@
+'use client'
+
+import { FaCheckCircle } from 'react-icons/fa'
+import { SubTitle, List, ListItem, Icon, Note, Schedule, ScheduleTime, StrongTitle, ScheduleWrapper, Pricing, PricingTag, PricingTitle, PricingWrapper, PricingSection, Space, AdditionalInfo } from './styles'
+
+export default function DetailInfo() {
+  return (
+    <div>
+      <div>
+        <SubTitle>What`s included when dive with us</SubTitle>
+        <List>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            2X Dives
+          </ListItem>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            Hotel Transfer
+          </ListItem>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            Lunch and Drinks
+          </ListItem>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            Ticket and Porter
+          </ListItem>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            Certified Instructor
+          </ListItem>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            Oxygen Tanks
+          </ListItem>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            Weight Belt
+          </ListItem>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            Speed Boat
+          </ListItem>
+          <ListItem>
+            <Icon>
+              <FaCheckCircle />
+            </Icon>
+            Snorkelling Set
+          </ListItem>
+        </List>
+        <Note>
+          Note: for open water course & discover scuba diving, full set dive equipments are inculded.
+        </Note>
+      </div>
+      <ScheduleWrapper>
+        <SubTitle>Departure & Return</SubTitle>
+        <Schedule>
+          <div>Pickup from your hotel</div>
+          <ScheduleTime>7.00 AM - 7.30 AM</ScheduleTime>
+        </Schedule>
+        <Schedule>
+          <div>Depart from Bali Stingray Dive Center</div>
+          <ScheduleTime>8.00 AM - 8.30 AM</ScheduleTime>
+        </Schedule>
+        <Schedule>
+          <div>Arrive in Gili Tepekong</div>
+          <ScheduleTime>10.00 AM - 10.30 AM</ScheduleTime>
+        </Schedule>
+        <Schedule>
+          <div>1st & 2nd dive</div>
+          <ScheduleTime>Morning Session</ScheduleTime>
+        </Schedule>
+        <Schedule>
+          <div>Lunch included on the speed boat</div>
+          <ScheduleTime>Afternoon</ScheduleTime>
+        </Schedule>
+        <Schedule>
+          <div>3rd dive</div>
+          <ScheduleTime>Afternoon Session</ScheduleTime>
+        </Schedule>
+        <Schedule>
+          <div>Return to Bali Stingray Dive Center</div>
+          <ScheduleTime>5.00 PM - 6.00 PM</ScheduleTime>
+        </Schedule>
+      </ScheduleWrapper>
+      <PricingSection>
+        <SubTitle className='uppercase'>Pricing</SubTitle>
+        <PricingWrapper>
+          <Pricing>
+            <PricingTitle>Certified Diver</PricingTitle>
+            <PricingTag>IDR 1.950.000</PricingTag>
+          </Pricing>
+          <Pricing>
+            <PricingTitle>Discover Scuba Diving</PricingTitle>
+            <PricingTag>IDR 2.300.000</PricingTag>
+          </Pricing>
+          <Pricing>
+            <PricingTitle>Snorkeling</PricingTitle>
+            <PricingTag>IDR 900.000</PricingTag>
+          </Pricing>
+        </PricingWrapper>
+      </PricingSection>
+      <AdditionalInfo>
+        <div>
+          <SubTitle>Additional Services</SubTitle>
+          <Space>
+            <StrongTitle>Private Divemaster</StrongTitle>
+            <div>IDR 750.000</div>
+          </Space>
+          <Space>
+            <StrongTitle>Fullset Dive Equipment Rental</StrongTitle>
+            <div>IDR 250.000</div>
+          </Space>
+          <Space>
+            <StrongTitle>Extra Dive</StrongTitle>
+            <div>IDR 450.000</div>
+          </Space>
+          <Space>
+            <StrongTitle>Dive Computer Rental</StrongTitle>
+            <div>IDR 150.000</div>
+          </Space>
+          <Space>
+            <StrongTitle>Professional Photography</StrongTitle>
+            <div>IDR 2.500.000</div>
+          </Space>
+        </div>
+        <div>
+          <SubTitle>Additional Information</SubTitle>
+          <Space>
+            <div>INT valve scuba tanks available, DIN valve provided on demand.</div>
+          </Space>
+          <Space>
+            <div>Nitrox provided on demand, additional Rp. 50,000 per tank.</div>
+          </Space>
+          <Space>
+            <div>Dive in a small group, maximum 3 divers (Guide Ratio 3:1).</div>
+          </Space>
+          <Space>
+            <div>Dive sites will be chosen based on diver experience.</div>
+          </Space>
+          <Space>
+            <div>Emergency Oxygen and First Aid ready.</div>
+          </Space>
+          <Space>
+            <div>We speak English & Bahasa Indonesia.</div>
+          </Space>
+        </div>
+      </AdditionalInfo>
+    </div>
+  )
+}
---
+++ src/app/(routes)/fun-diving/gili-tepekong/(sections)/Hero/styles.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+import theme from '@/styles/theme';
+
+export const Section = styled.section`
+  position: relative;
+  height: 500px;
+  overflow: hidden;
+  background-image: url('/assets/images/nusa-penida.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  @media screen and (max-width: 1024px) {
+    height: 350px;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  z-index: 2;
+  align-items: center;
+  justify-content: center;
+  color: ${theme.colors.white};
+  height: 100%;
+
+  @media screen and (max-width: 1024px) {
+    justify-content: flex-end;
+    align-items: flex-start;
+    padding-bottom: 30px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 900;
+  margin: 0;
+
+  @media screen and (max-width: 1024px) {
+    font-size: 32px;
+  }
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 24px;
+  font-weight: 400;
+  margin: 20px 0 0;
+  max-width: 640px;
+  text-align: center;
+  line-height: 1.2;
+
+  @media screen and (max-width: 1024px) {
+    font-size: 18px;
+    text-align: left;
+  }
+`;

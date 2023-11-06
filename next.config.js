db265d9399877d6d6720c8d29b3db8170152bd83
dhantosy <dhantosy@gmail.com>
2023-11-06 15:30:59 +0800

    feat: added redirects

--- next.config.js
+++ next.config.js
@@ -2,7 +2,22 @@
 const nextConfig = {
   compiler: {
     styledComponents: true,
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/fun-diving',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/dive-courses',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+  poweredByHeader: false,
 }
 
 module.exports = nextConfig
